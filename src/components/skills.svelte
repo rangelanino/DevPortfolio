95dfad9bb398afa16370df77e712802c2ffa5060
unknown <jerico.miggy.rangel@gmail.com>
2023-02-11 23:17:29 +0800

    Home page: initial static layout components

---
+++ src/components/skills.svelte
@@ -0,0 +1,56 @@
+<div id="container" class="py-12 mx-auto">
+    <h1 style="font-size: 32px"> Core Competencies</h1>
+    <div class="flex space-x-10">
+    <ul>
+        <li id="head"> Programming Languages </li>
+        {#each languages as lang, i}
+        <li> {lang} </li>
+        {/each}
+    </ul>
+    <ul>
+        <li id="head">Data Analytics and ETL</li>
+        {#each data_engg as engg, i}
+        <li> {engg} </li>
+        {/each}
+    </ul>
+    <ul>
+        <li id="head">Dev Tools</li>
+        {#each dev_tools as tool, i}
+        <li> {tool} </li>
+        {/each}
+    </ul>
+    </div>
+</div>
+
+<script>
+const languages = ["Python", "SQL", "Vue", "Typescript", 
+                   "Latex", "Bash"];
+const data_engg = ["HighCharts", "DuckDB", "Apache Spark", 
+                   "Pandas", "Matplotlib", "Seaborn"];
+const dev_tools = ["Docker", "Airflow", "Kubernetes", 
+                   "ArgoCD", "CircleCI", "WebPack"]
+</script>
+
+<style lang="postcss">
+
+#container{
+    display: table;
+  margin-right: auto;
+  margin-left: auto;
+
+}
+
+h1{
+    font-size: larger;
+    font-weight: 900;
+}
+
+li {
+    text-align: center;
+}
+
+#head {
+    font-size: larger;
+    font-weight: 800;
+}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script>
+    import Sideicons from '../components/sideicons.svelte';
+    import Header from '../components/header.svelte';
+    import Skills from '../components/skills.svelte';
+</script>
+
+<Header />
+<body class="bg-gradient-to-r from-indigo-200 to-indigo-400"> 
+    <div class="flex items-center justify-center">        
+        <h1 id="lastname" class="ml-5 pt-3">RANGEL</h1>
+    </div>
+    <div class="flex items-center justify-center">
+        <h3>Physicist turned <i>Data Engineer</i></h3>
+    </div>
+    <Sideicons />
+    <Skills />
+</body>
+
+<style lang="postcss">
+
+#lastname{
+    font-size: 83px;
+    font-weight: 1000;
+    color: black;
+}   
+
+</style>
